0dacd8e4131b5d59dd6d92713dfbbc469a297ca8
researchmind-dev <80063025+researchmind-dev@users.noreply.github.com>
2026-09-25 00:06:41 +0530

    Add: 5. Project using Ridge Lasso Elastic

---
+++ 5-Step By Step Project Implementation With LifeCycle Of ML Projects/forestfire-main/application.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+app=application
+
+## import ridge regresor model and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
+
+## Route for home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+# render_template always look for template folder and want toi see its arg name files there 
+
+@app.route('/predictdata',methods=['GET','POST']) ## route to prediction page
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',result=result[0])
+
+    else:
+        return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+    # this 0.0.0.0 map to localhost of any machine currently its running on
